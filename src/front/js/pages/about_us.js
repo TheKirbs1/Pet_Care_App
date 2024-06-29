598f28db020e09226747ffcd1224585f15078ed4
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-06-29 23:01:15 +0000

    added about,edit,favorit,login,pet_details,pet_registration,private,signup pages and routes inside layout

---
+++ src/front/js/pages/about_us.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const About_us = () => {
+
+    return (
+        <>
+            About us!
+        </>
+)};
+
---
+++ src/front/js/pages/favorite.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const Favorite = () => {
+
+    return (
+        <>
+            Favorite
+        </>
+)};
