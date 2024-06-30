d15a91906012fd2ac11910a501ef6537e0167d84
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-06-30 03:59:30 +0000

    made dropdown menu a profile picture svg placeholder

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,22 @@ import React, {useContext} from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import simpleLogo from "../../img/simpleLogo.png";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light sticky-top">
 			<div className="container">
 				<Link to="/">
-					<img src={simpleLogo} className="img-fluid" style={{ height: 100 }} alt="Pet Logo/Home Button" />
+					<img src={simpleLogo} className="img-fluid" style={{ height: 80 }} alt="Pet Logo/Home Button" />
 				</Link>
 				<ul>
 					<li className="nav-item dropdown">
 					<a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Dropdown link
+						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+						<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+						<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+						</svg>
 					</a>
 					<ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 						<li><Link className="dropdown-item" to="/login">Login</Link></li>
