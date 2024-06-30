ffa35916761ba20d6fdd9a2127e41543add09098
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-06-30 03:48:59 +0000

    Navbar logo with home button functionality implemented and working, drop down menu created and Login,Favorites,Account Settings, About us added and functioning

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import simpleLogo from "../../img/simpleLogo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light bg-light sticky-top">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={simpleLogo} className="img-fluid" style={{ height: 100 }} alt="Pet Logo/Home Button" />
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+				<ul>
+					<li className="nav-item dropdown">
+					<a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Dropdown link
+					</a>
+					<ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<li><Link className="dropdown-item" to="/login">Login</Link></li>
+						<li><Link className="dropdown-item" to="/favorite">Favorites</Link></li>
+						<li><Link className="dropdown-item" to="/account_settings">Account Settings</Link></li>
+						<li><Link className="dropdown-item" to="/about_us">About us</Link></li>
+					</ul>
+					</li>
+			</ul>
 			</div>
 		</nav>
 	);
