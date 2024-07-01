59889fdc013a9e4faa60f4686710f9cc69a9e189
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-07-01 23:35:22 +0000

    authentication for private page

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,10 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Private = () => {
+    const {store, actions} = useContext(Context);
+    const [authStatus, setAuthStatus] = useState("Pending");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const authenticate = async () => {
+        let result = await actions.getDogs();
+        if (result) {
+            setAuthStatus("granted")
+        } else { setAuthStatus("denied") }
+      }
+      authenticate()
+    }, [actions])
 
     return (
-        <>
-            Private
-        </>
+      <>
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </>
 )};
