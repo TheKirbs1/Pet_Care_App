8cdf98f90a717ccfe156e7a8961fe982181ba4b6
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-07-03 06:08:32 +0000

    fixed avatar position, added new buttons, all working

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,11 +1,13 @@
-import React, { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/private.css";
 
 export const Private = () => {
     const {store, actions} = useContext(Context);
     //const [authStatus, setAuthStatus] = useState("Pending");
-    const navigate = useNavigate();
+    
+    
 
     //useEffect(() => {
     //  const authenticate = async () => {
@@ -18,66 +20,49 @@ export const Private = () => {
     //}, [actions])
 
     return (
-      <>
-        <div className="container">
-            <div className="main-body">
-                <div>
-                  <button className="btn mt-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                      <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                    </svg>
-                  </button>
-                </div>
-                <div className="row gutters-sm">
-                    <div className="col-md-4 mb-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="d-flex flex-column align-items-center text-center">
-                                    <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="avatar" className="rounded-circle" width="150" />
-                                    <div className="mt-3">
-                                        <h4>John Doe</h4>
-                                        <p className="text-secondary mb-1">Full Stack Developer</p>
-                                        <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                                        <button className="btn btn-primary">Follow</button>
-                                        <button className="btn btn-outline-primary">Message</button>
-                                    </div>
-                                </div>
-                            </div>
+        <>
+            <div className="profile-container">
+                <div className="row justify-content-center">
+                    <div className="col-md-4">
+                    <Link to="edit_pet">
+                    <button className="btn mt-4 edit-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                        </svg>
+                    </button>
+                </Link>
+                    </div>
+                    <div className="col-md-4 ml-auto mr-auto">
+                        <div className="profile-picture d-flex flex-column justify-items-center align-items-center">
+                            <img
+                            src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
+                            alt="profile picture"
+                            className="img-fluid rounded-circle w-50"
+                            />
                         </div>
-                        <div className="card mt-3">
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                                    <span className="text-secondary">https://bootdey.com</span>
-                                </li>
-                                {/* Additional list items */}
-                            </ul>
+                        <div className="name justify-content-center align-items-center d-flex mt-3">
+                            <h3 className="title">Pet Name</h3>
                         </div>
                     </div>
-                    <div className="col-md-8">
-                        <div className="card mb-3">
-                            <div className="card-body">
-                                {/* Profile information */}
-                                <div className="row">
-                                    {/* Individual rows */}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row gutters-sm">
-                            <div className="col-sm-6 mb-3">
-                                <div className="card h-100">
-                                    <div className="card-body">
-                                        {/* Project status */}
-                                        <h6 className="d-flex align-items-center mb-3"><i className="material-icons text-info mr-2">assignment</i>Project Status</h6>
-                                        {/* Progress bars */}
-                                    </div>
-                                </div>
-                            </div>
-                            {/* Additional columns */}
+                    <div className="col-md-4">
+                        <div className="right-buttons">
+                            <Link to="pet_registration">
+                                <button className="btn mt-4 add-pet text-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                                    </svg>
+                                    <p className="mt-1">Add a Pet</p>
+                                </button>
+                            </Link>
+                            <button className="btn text-primary switch-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5"/>
+                                </svg>
+                                <p className="mt-1">Switch Pet</p>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-      </>
+        </>
 )};
