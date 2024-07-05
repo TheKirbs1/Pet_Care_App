6dc7b1a347bbeb63e03e240113f1509446e63bcb
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-07-05 05:22:44 +0000

    added gender buttons

--- src/front/js/pages/pet_registration.js
+++ src/front/js/pages/pet_registration.js
@@ -10,22 +10,48 @@ export const Pet_registration = () => {
         } 
     }
     console.log(img)
+    const [chosenColor, setChosenColor] = useState("")
+
     return (
         <>
             <div className="pet-registration">
                 <h2 className="text-center my-5">Pet Information</h2>
                 <div className="form-container">
                     <form>
-                    <input type="file" className="form-control" id="imageUpload" aria-describedby="emailHelp" onChange={(e)=>handleUpload(e)}/> 
+                    <input type="file" className="form-control mb-3" id="imageUploader" aria-describedby="uploader" onChange={(e)=>handleUpload(e)}/> 
                     {img && <img src={img} height="200" width="200" alt="Uploaded Preview" />}
                             <div className="mb-3 fw-bold">
-                                <label htmlFor="exampleInputEmail1" className="form-label">Dog name</label>
-                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                                <label htmlFor="dogName" className="form-label">Dog name</label>
+                                <input type="text" className="form-control" id="dogName" aria-describedby="dogName"/>
                             </div>
                             <div className="mb-3 fw-bold">
-                                <label htmlFor="exampleInputPassword1" className="form-label">Breed</label>
-                                <input type="text" className="form-control" id="exampleInputPassword1"/>
+                                <label htmlFor="dogBreed" className="form-label">Breed</label>
+                                <input type="text" className="form-control" id="dogBreed"/>
                             </div>
+                            <div className="gender-buttons mb-3">
+                                <input
+                                    type="radio"
+                                    className="btn-check"
+                                    name="options-outlined"
+                                    id="success-outlined"
+                                    autoComplete="off"
+                                    defaultChecked=""
+                                />
+                                <label className="btn btn-outline-warning me-1 mb-3 px-5" htmlFor="success-outlined">
+                                    Male
+                                </label>
+                                <input
+                                    type="radio"
+                                    className="btn-check"
+                                    name="options-outlined"
+                                    id="danger-outlined"
+                                    autoComplete="off"
+                                />
+                                <label className="btn btn-outline-warning mb-3 px-5" htmlFor="danger-outlined">
+                                    Female
+                                </label>
+                            </div>
+
                             <button type="submit" className="btn btn-primary save-button">Save</button>
                     </form>
                 </div>
