7569238c7ac62dd54cfb61a5fd53dd444ce69e1c
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-07-05 07:29:33 +0000

    added weight buttons. needs more styling

--- src/front/js/pages/pet_registration.js
+++ src/front/js/pages/pet_registration.js
@@ -16,10 +16,9 @@ export const Pet_registration = () => {
         <>
             <div className="pet-registration">
                 <h2 className="text-center my-5">Pet Information</h2>
-                <div className="form-container">
-                    <form>
-                    <input type="file" className="form-control mb-3" id="imageUploader" aria-describedby="uploader" onChange={(e)=>handleUpload(e)}/> 
-                    {img && <img src={img} height="200" width="200" alt="Uploaded Preview" />}
+                <div className="row">
+                    <div className="col-md-6">
+                        <form>
                             <div className="mb-3 fw-bold">
                                 <label htmlFor="dogName" className="form-label">Dog name</label>
                                 <input type="text" className="form-control" id="dogName" aria-describedby="dogName"/>
@@ -28,32 +27,112 @@ export const Pet_registration = () => {
                                 <label htmlFor="dogBreed" className="form-label">Breed</label>
                                 <input type="text" className="form-control" id="dogBreed"/>
                             </div>
+                            
                             <div className="gender-buttons mb-3">
                                 <input
                                     type="radio"
                                     className="btn-check"
                                     name="options-outlined"
-                                    id="success-outlined"
+                                    id="male-outlined"
                                     autoComplete="off"
                                     defaultChecked=""
                                 />
-                                <label className="btn btn-outline-warning me-1 mb-3 px-5" htmlFor="success-outlined">
+                                <label className="btn btn-outline-warning me-3 mb-3 px-5" htmlFor="male-outlined">
                                     Male
                                 </label>
                                 <input
                                     type="radio"
                                     className="btn-check"
                                     name="options-outlined"
-                                    id="danger-outlined"
+                                    id="female-outlined"
                                     autoComplete="off"
                                 />
-                                <label className="btn btn-outline-warning mb-3 px-5" htmlFor="danger-outlined">
+                                <label className="btn btn-outline-warning mb-3 px-5" htmlFor="female-outlined">
                                     Female
                                 </label>
                             </div>
-
-                            <button type="submit" className="btn btn-primary save-button">Save</button>
-                    </form>
+                        </form>
+                    </div>
+                    <div className="col-md-6">
+                        <form>
+                            <input type="file" className="form-control mb-3" id="imageUploader" aria-describedby="uploader" onChange={(e)=>handleUpload(e)}/> 
+                            {img && <img src={img} height="200" width="200" alt="Uploaded Preview" />}
+                            <div className="spayed-neutered mb-3">
+                                <input
+                                    type="radio"
+                                    className="btn-check"
+                                    name="options-outlined"
+                                    id="yes-outlined"
+                                    autoComplete="off"
+                                    defaultChecked=""
+                                />
+                                <label className="btn btn-outline-warning me-3 mb-3 px-5" htmlFor="yes-outlined">
+                                    Yes
+                                </label>
+                                <input
+                                    type="radio"
+                                    className="btn-check"
+                                    name="options-outlined"
+                                    id="no-outlined"
+                                    autoComplete="off"
+                                />
+                                <label className="btn btn-outline-warning mb-3 px-5" htmlFor="no-outlined">
+                                    No
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md">
+                        <div className="weight-buttons mb-3">
+                            <input
+                                type="radio"
+                                className="btn-check"
+                                name="options-outlined"
+                                id="option1-outlined"
+                                autoComplete="off"
+                                defaultChecked=""
+                            />
+                            <label className="btn btn-outline-warning me-3 mb-3 px-5" htmlFor="option1-outlined">
+                                0-25lbs
+                            </label>
+                            <input
+                                type="radio"
+                                className="btn-check"
+                                name="options-outlined"
+                                id="option2-outlined"
+                                autoComplete="off"
+                            />
+                            <label className="btn btn-outline-warning me-3 mb-3 px-5" htmlFor="option2-outlined">
+                                25-50lbs
+                            </label>
+                            <input
+                                type="radio"
+                                className="btn-check"
+                                name="options-outlined"
+                                id="option3-outlined"
+                                autoComplete="off"
+                            />
+                            <label className="btn btn-outline-warning me-3 mb-3 px-5" htmlFor="option3-outlined">
+                                50-100lbs
+                            </label>
+                            <input
+                                type="radio"
+                                className="btn-check"
+                                name="options-outlined"
+                                id="option4-outlined"
+                                autoComplete="off"
+                            />
+                            <label className="btn btn-outline-warning mb-3 px-5" htmlFor="option4-outlined">
+                                100lbs +
+                            </label>
+                        </div>
+                       
+                    </div>
+                </div>
+                <div className="text-center">
+                <button type="btn" className="btn btn-primary save-button">Save</button>
                 </div>
             </div>
         </>
