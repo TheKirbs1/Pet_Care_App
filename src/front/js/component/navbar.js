8acd25674a84238947129c4cf0d63f0dd99db2be
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-07-10 23:27:50 +0000

    styles added

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
 	const { store, actions } = useContext(Context)
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar">
 			<div className="nav-container container-fluid">
 				<Link to="/" id="home-logo">
 					<div className="logo-div">
@@ -29,7 +29,7 @@ export const Navbar = () => {
 					<ul>
 						<li className="nav-item dropdown">
 							<a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-								<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+								<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="rgb(55, 58, 64)" className="bi bi-person-circle" viewBox="0 0 16 16">
 									<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
 									<path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
 								</svg>
