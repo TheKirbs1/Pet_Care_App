32bf5e0eeedba0b272883588197d4ffe3596cc93
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-07-17 15:33:07 +0000

    all pictures fixed on dog breed cards

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -51,13 +51,23 @@ export const Home = () => {
             }));
             const pitbull = dogsWithDefaultBredFor.find(dog => dog.name === "American Pit Bull Terrier");
             if (pitbull) {
-            pitbull.image_url = "https://cdn2.thedogapi.com/images/HkC31gcNm.png";
-        }
+                pitbull.image_url = "https://cdn2.thedogapi.com/images/HkC31gcNm.png";
+            }
+            const greatPyrenees = dogsWithDefaultBredFor.find(dog => dog.name === "Great Pyrenees");
+            if (greatPyrenees) {
+                greatPyrenees.image_url = "https://cdn2.thedogapi.com/images/B12uzg9V7.png";
+            }
+            const saintBernard = dogsWithDefaultBredFor.find(dog => dog.name === "Saint Bernard");
+            if (saintBernard) {
+                saintBernard.image_url = "https://cdn2.thedogapi.com/images/_Qf9nfRzL.png";
+            }
+    
             setDogs(dogsWithDefaultBredFor);
         } catch (error) {
             console.error(error);
         }
     };
+    
 
     const filterSearch = async () => {
         try {
@@ -92,7 +102,7 @@ export const Home = () => {
                     <div className="row g-0">
                         <div className="col-md-4"></div>
                         <div className="col-md-2">
-                        <img src={header_pups} className="headerImg img-fluid rounded-start" alt="Dog and its pup playing in a field" />
+                            <img src={header_pups} className="headerImg img-fluid rounded-start" alt="Dog and its pup playing in a field" />
                         </div>
                         <div className="col-md-2">
                         <div className="header-card-body">
@@ -129,25 +139,25 @@ export const Home = () => {
                 </form>
 
                 <div className="card-container">
-                    {!displaySearch && currentPage === 0 && mixedCards.map((card) => (
-                        <div className="card" key={card.id} style={{ width: "18rem" }}>
-                            <img src={card.imgSrc} className="card-img-top" alt={card.title} />
-                            <div className="card-body">
-                                <h5 className="card-title">{card.title}</h5>
-                                <div className="bredFor"><p className="card-text mb-2"><strong>Bred For:</strong> {card.bredFor}</p></div>
-                                <div className="card-buttons">
-                                    <Link to={card.link}>
-                                        <button type="button" className="learnMoreBtn btn btn-secondary">Learn More</button>
-                                    </Link>
-                                    <button className="favBtn btn btn btn-text-color mx-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
-                                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                                        </svg>
-                                    </button>
+                        {!displaySearch && currentPage === 0 && mixedCards.map((card) => (
+                            <div className="card" key={card.id} style={{ width: "18rem" }}>
+                                <img src={card.imgSrc} className="card-img-top" alt={card.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{card.title}</h5>
+                                    <div className="bredFor"><p className="card-text mb-2"><strong>Bred For:</strong> {card.bredFor}</p></div>
+                                    <div className="card-buttons">
+                                        <Link to={card.link}>
+                                            <button type="button" className="learnMoreBtn btn btn-secondary">Learn More</button>
+                                        </Link>
+                                        <button className="favBtn btn btn btn-text-color mx-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
+                                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
 
                     {currentPageData.map((dog) => (
                         <div key={dog.id} className="card" style={{ width: "18rem" }}>
@@ -158,44 +168,47 @@ export const Home = () => {
                             />
                             <div className="card-body">
                                 <h5 className="card-title">{dog.name}</h5>
-                                <div className="bredFor"><p className="card-text mb-2"><strong>Bred For:</strong> {dog.bred_for}</p></div>
+                                <div className="bredFor">
+                                    <p className="card-text mb-2"><strong>Bred For:</strong> {dog.bred_for}</p>
+                                </div>
                                 <div className="card-buttons">
-                                <Link to={`/${dog.name}`}>
-                                    <button type="button" className="learnMoreBtn btn btn-secondary">Learn More</button>
-                                </Link>
-                                <button className="favBtn btn btn-text-color mx-4">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
-                                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                                    </svg>
-                                </button>
+                                    <Link to={`/${dog.name}`}>
+                                        <button type="button" className="learnMoreBtn btn btn-secondary">Learn More</button>
+                                    </Link>
+                                    <button className="favBtn btn btn-text-color mx-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
+                                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                                        </svg>
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     ))}
+
                 </div>
 
 
-            <div>
-                <ReactPaginate
-                    previousLabel={'previous'}
-                    nextLabel={'next'}
-                    breakLabel={'...'}
-                    pageCount={Math.ceil(dogs.length / perPage)}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={3}
-                    onPageChange={handlePaginationClick}
-                    containerClassName={'pagination justify-content-center'}
-                    pageClassName={'page-item'}
-                    pageLinkClassName={'page-link'}
-                    previousClassName={'page-item'}
-                    previousLinkClassName={'page-link'}
-                    nextClassName={'page-item'}
-                    nextLinkClassName={'page-link'}
-                    breakClassName={'page-item'}
-                    breakLinkClassName={'page-link'}
-                    activeClassName={'active'}
-                />
-            </div>
+                <div className="paginate">
+                    <ReactPaginate
+                        previousLabel={'previous'}
+                        nextLabel={'next'}
+                        breakLabel={'...'}
+                        pageCount={Math.ceil(dogs.length / perPage)}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={3}
+                        onPageChange={handlePaginationClick}
+                        containerClassName={'pagination justify-content-center'}
+                        pageClassName={'page-item'}
+                        pageLinkClassName={'page-link'}
+                        previousClassName={'page-item'}
+                        previousLinkClassName={'page-link'}
+                        nextClassName={'page-item'}
+                        nextLinkClassName={'page-link'}
+                        breakClassName={'page-item'}
+                        breakLinkClassName={'page-link'}
+                        activeClassName={'active'}
+                    />
+                </div>
             </div>
             </section>
         </>
