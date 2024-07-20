eda4cb8605b9f08d9acc42f6f46b5a282df92dfc
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-07-20 22:52:13 +0000

    styling

--- src/front/js/pages/account_settings.js
+++ src/front/js/pages/account_settings.js
@@ -87,47 +87,40 @@ export const Account_settings = () => {
     };
 
     return (
-        <>
-            <div className=" fullFormContainer box border border-1 mx-5 my-5" >
-                <div className="d-flex justify-content-center  vstack my-5 mx-5 ">
-                    <div><h1>ACCOUNT SETTINGS</h1></div>
+        <div className="fullDiv">
+            <div><h1 className="acountSettingsTitle d-flex justify-content-center align-content-center text-decoration-underline" >ACCOUNT SETTINGS</h1></div>
+            <div className="d-flex justify-content-center align-content-center box mx-5 my-5 pb-2 vstack rounded-2" >
+                <div className="userSettingsDiv vstack mt-5 mx-5 border">
 
-                    <div>
-                        <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="100" cy="100" r="98" fill="#ffffff" stroke="#e0e0e0" stroke-width="2" />
-                            <line x1="90" y1="100" x2="110" y2="100" stroke="#999999" stroke-width="4" stroke-linecap="round" />
-                            <line x1="100" y1="90" x2="100" y2="110" stroke="#999999" stroke-width="4" stroke-linecap="round" />
-                        </svg>
-                    </div>
+                    <p className="d-flex justify-content-center align-items-center">USER SETTINGS</p>
 
 
-                    <div className="w-full max-w-md">
+                    <div className="w-full max-w-md ">
 
-                        <div className="mb-1 vstack">
-                            <label className="block mb-2 ">Edit Email:</label>
+                        <div className="mb-1 vstack px-2">
+                            <label className="block mb-2">Edit Email:</label>
                             <input
                                 type="email"
                                 placeholder="Enter your email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                                style={{ width: '100px', minWidth: '500px' }}
-
+                                style={{ width: '100px', minWidth: '1000px' }}
                             />
                             {emailError && <div className="text-danger">{emailError}</div>}
                         </div>
-                        <div className="mb-1 vstack">
+                        <div className="mb-1 vstack px-2">
                             <label className="block mb-2 font-bold">Edit Password:</label>
                             <input
                                 type="password"
                                 placeholder="Enter your password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
-                                style={{ width: '100px', minWidth: '500px' }}
+                                style={{ width: '100px', minWidth: '1000px' }}
                             />
                             {passwordError && <div className="text-danger">{passwordError}</div>}
                         </div>
                     </div>
-                    <div className="d-flex justify-content-start align-content-start pt-1 mb-2">
+                    <div className="d-flex justify-content-end align-content-center pt-1 mb-2 mx-3">
                         <button type="button" className="save-btn btn-primary text-dark"
                             style={{ width: '100px', minWidth: '100px' }}
                             onClick={handleEditUserSettings}>
@@ -136,61 +129,66 @@ export const Account_settings = () => {
                     </div>
                 </div>
 
-                <div className="d-flex justify-content-center vstack mx-5">
-                    <h1>MY PETS</h1>
-                    <b>------------------------------------------------------------------------------</b>
-                    {dogs ? (
-                        dogs.length > 0 ? (
-                            dogs.map((dog, index) => (
-                                <Link key={dog.id} to={"/private/edit_pet/" + dog.id} className="d-flex">
-                                    <h6><button type="button" className="btn btn-link">{dog.name}</button></h6>
-                                </Link>
-                            ))
+                <div className="myPetsDiv d-flex justify-content-center vstack mx-5 mt-5 pb-4 border">
+                    <p className="d-flex justify-content-center ">MY PETS</p>
+                    <div className="border border-dark border-start-0 border-end-0 border-3 mx-4">
+                        {dogs ? (
+                            dogs.length > 0 ? (
+                                dogs.map((dog, index) => (
+                                    <Link key={dog.id} to={"/private/edit_pet/" + dog.id} className="d-flex">
+                                        <h6><button type="button" className="btn btn-link">{dog.name}</button></h6>
+                                    </Link>
+                                ))
+                            ) : (
+                                <div className="alert alert-transparent d-flex justify-content-center">No pets found. Add a new pet to get started!</div>
+                            )
                         ) : (
-                            <div className="alert alert-transparent">No pets found. Add a new pet to get started!</div>
-                        )
-                    ) : (
-                        <div className="text-center">
-                            <div className="spinner-border" role="status">
-                                <span className="visually-hidden">Loading...</span>
+                            <div className="text-center">
+                                <div className="spinner-border" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
 
-                    <b>------------------------------------------------------------------------------</b>
+                    </div>
                 </div>
 
-                <div className="d-flex justify-content-start mt-3 px-5">
-                    <h6 className="text-dark me-4">{store.isAccountActive ? "Deactivate Account" : "Reactivate Account"}</h6>
-                    <button
-                        type="button"
-                        className="btn btn-dark"
-                        data-bs-toggle="modal"
-                        data-bs-target="#exampleModal"
-                    >
-                        {store.isAccountActive ? <i className="fa-solid fa-circle"></i> : <i className="fa-solid fa-bone"></i>}
-                    </button>
+                <div className="profileSettings mx-5 mt-5 border border-rounded">
+                    <div className="d-flex justify-content-center mt-2"><p>Profile Settings</p></div>
 
-                    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h1 className="modal-title fs-5" id="exampleModalLabel">{store.isAccountActive ? "Account Deactivation" : "Account Reactivation"}</h1>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div className="modal-body">
-                                    {store.isAccountActive
-                                        ? "Are you sure you want to deactivate your account?"
-                                        : "Are you sure you want to reactivate your account?"}
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                    <button
-                                        type="button"
-                                        className="btn btn-danger"
-                                        onClick={store.isAccountActive ? handleDeactivation : handleReactivation}>
-                                        {store.isAccountActive ? "Deactivate" : "Reactivate"}
-                                    </button>
+                    <div className="d-flex justify-content-end mt-3 px-5 mb-5">
+
+
+                        <button
+                            type="button"
+                            className="btn btn-light text-dark btn-outline-danger btn-sm rounded-0"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                        >
+                            {store.isAccountActive ? "DEACTIVATE ACCOUNT" : "REACTIVATE ACCOUNT"}
+                        </button>
+
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h1 className="modal-title fs-5" id="exampleModalLabel">{store.isAccountActive ? "Account Deactivation" : "Account Reactivation"}</h1>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        {store.isAccountActive
+                                            ? "Are you sure you want to deactivate your account?"
+                                            : "Are you sure you want to reactivate your account?"}
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                        <button
+                                            type="button"
+                                            className="btn btn-danger"
+                                            onClick={store.isAccountActive ? handleDeactivation : handleReactivation}>
+                                            {store.isAccountActive ? "Deactivate" : "Reactivate"}
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -199,7 +197,7 @@ export const Account_settings = () => {
 
 
             </div>
-        </>
+        </div>
     );
 };
 
