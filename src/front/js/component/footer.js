845d18e11b2697691245088ace3d0421c820db07
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-07-21 02:17:58 +0000

    icons added

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,19 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../styles/footer.css";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 px-3 text-center text-body-secondary mt-2">
-		
-		<div className="hstack justify-content-between text-secondary">
-			<div className="d-flex align-items-center text-body-secondary">
-			<b className="text-dark">STAY IN THE KNOW</b>
-				<a href="/about_us" className="text-primary mx-2"><i className="fa-solid fa-address-card style=color: #000000;"></i></a>
-				<a href="mailto:pal4Pets@doglover.com?subject=Hello&body=This is the email body." className="text-primary"><i className="fa-solid fa-envelope style=color: #000000;"></i></a>
-			</div>
-			<div className="text-body-secondary">
-				<div className="text-body-secondary"><h6>@ 2024 ERROR 500 <i className="fa-regular fa-copyright text-body-secondary"> </i></h6></div>
-			</div>
-		</div>
-	</footer>
-
+  <footer className="footer py-3 px-3 text-center text-body-secondary">
+    <div className="hstack justify-content-between text-secondary">
+      <div className="d-flex align-items-center text-body-secondary">
+        <b className="text-dark">STAY IN THE KNOW</b>
+        <a href="/about_us" className="text-primary mx-2">
+          <span className="icon-circle">
+            <i className="fa-solid fa-address-card"></i>
+          </span>
+        </a>
+        <a href="mailto:pal4Pets@doglover.com?subject=Hello&body=This is the email body." className="text-primary">
+          <span className="icon-circle">
+            <i className="fa-solid fa-envelope"></i>
+          </span>
+        </a>
+        <a href="/terms_of_use" className="text-primary mx-1">
+          <span className="icon-circle">
+            <i className="fa-solid fa-gavel"></i>
+          </span>
+        </a>
+        <a href="/privacy_policy" className="text-primary mx-1">
+          <span className="icon-circle">
+            <i className="fa-solid fa-scroll"></i>
+          </span>
+        </a>
+      </div>
+      <div className="text-body-secondary">
+        <div className="text-body-secondary">
+          <h6>@ 2024 ERROR 500 <i className="fa-regular fa-copyright text-body-secondary"></i></h6>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
+
