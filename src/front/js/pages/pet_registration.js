182cd7a6fd95a40d0898ff8a09c192101a3d13e0
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-07-21 17:53:49 +0000

    styled input texts and buttons, trying background

--- src/front/js/pages/pet_registration.js
+++ src/front/js/pages/pet_registration.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
+// import background from "../../img/background11.png";
 import "../../styles/pet_registration.css";
 
 export const Pet_registration = () => {
@@ -57,229 +58,231 @@ export const Pet_registration = () => {
     }
 
     return (
-        <form className="petRegistrationForm row g-4">
-            <h1 className="form-title d-flex justify-content-center">
-                Add your pet
-            </h1>
-            <div className="col-md-12 d-flex align-items-center">
-                <div className="image-container me-3">
-                    {uploadImage ? (
-                        <img
-                            src={URL.createObjectURL(uploadImage)}
-                            height="200"
-                            width="200"
-                            alt="Uploaded Preview"
-                            className="rounded-circle"
+        <div className="formContainer">
+            <form className="petRegistrationForm row g-4">
+                <h1 className="form-title d-flex justify-content-center">
+                    Add your pet
+                </h1>
+                <div className="col-md-12 d-flex align-items-center">
+                    <div className="image-container me-3">
+                        {uploadImage ? (
+                            <img
+                                src={URL.createObjectURL(uploadImage)}
+                                height="200"
+                                width="200"
+                                alt="Uploaded Preview"
+                                className="rounded-circle"
+                            />
+                        ) : (
+                            <img
+                                src="https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg"
+                                height="200"
+                                width="200"
+                                alt="Placeholder"
+                                className="rounded-circle"
+                            />
+                        )}
+                    </div>
+                    <div className="file-input-container">
+                        <label htmlFor="imageInput" className="form-label">
+                            Upload picture
+                        </label>
+                        <input
+                            type="file"
+                            className="form-control"
+                            id="imageInput"
+                            aria-describedby="inputGroupFileAddon04"
+                            aria-label="Upload"
+                            onChange={handleImageUpload}
                         />
-                    ) : (
-                        <img
-                            src="https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg"
-                            height="200"
-                            width="200"
-                            alt="Placeholder"
-                            className="rounded-circle"
-                        />
-                    )}
+                    </div>
                 </div>
-                <div className="file-input-container">
-                    <label htmlFor="imageInput" className="form-label">
-                        Upload picture
+                <div className="col-md-6">
+                    <label htmlFor="dogName" className="form-label">
+                        Name
                     </label>
                     <input
-                        type="file"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        type="text"
                         className="form-control"
-                        id="imageInput"
-                        aria-describedby="inputGroupFileAddon04"
-                        aria-label="Upload"
-                        onChange={handleImageUpload}
+                        id="dogName"
                     />
                 </div>
-            </div>
-            <div className="col-md-6">
-                <label htmlFor="dogName" className="form-label">
-                    Name
-                </label>
-                <input
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                    type="text"
-                    className="form-control"
-                    id="dogName"
-                />
-            </div>
-            <div className="col-md-6">
-                <label htmlFor="dogBreed" className="form-label">
-                    Breed
-                </label>
-                <input
-                    onChange={(e) => setBreed(e.target.value)}
-                    value={breed}
-                    type="text"
-                    className="form-control"
-                    id="dogBreed"
-                />
-            </div>
-            <div className="col-md-6">
-                <label htmlFor="birthday" className="form-label">
-                    Date of Birth
-                </label>
-                <input
-                    onChange={(e) => setBirth(e.target.value)}
-                    type="date"
-                    className="form-control"
-                    id="birthday"
-                />
-            </div>
-            <div className="col-md-6">
-                <div className="weight-buttons mb-3 text-center">
-                    <p>Weight</p>
-                    <input
-                        onChange={() => setWeight("0-25 lbs")}
-                        type="radio"
-                        value="0-25 lbs"
-                        className="btn-check"
-                        name="weight-options"
-                        id="option1-outlined"
-                        autoComplete="off"
-                    />
-                    <label
-                        className="btn btn-outline-warning me-3 mb-3 px-5"
-                        htmlFor="option1-outlined"
-                    >
-                        0-25lbs
+                <div className="col-md-6">
+                    <label htmlFor="dogBreed" className="form-label">
+                        Breed
                     </label>
                     <input
-                        onChange={() => setWeight("25-50 lbs")}
-                        type="radio"
-                        value="25-50 lbs"
-                        className="btn-check"
-                        name="weight-options"
-                        id="option2-outlined"
-                        autoComplete="off"
+                        onChange={(e) => setBreed(e.target.value)}
+                        value={breed}
+                        type="text"
+                        className="form-control"
+                        id="dogBreed"
                     />
-                    <label
-                        className="btn btn-outline-warning me-3 mb-3 px-5"
-                        htmlFor="option2-outlined"
-                    >
-                        25-50lbs
-                    </label>
-                    <input
-                        onChange={() => setWeight("50-100 lbs")}
-                        type="radio"
-                        value="50-100 lbs"
-                        className="btn-check"
-                        name="weight-options"
-                        id="option3-outlined"
-                        autoComplete="off"
-                    />
-                    <label
-                        className="btn btn-outline-warning me-3 mb-3 px-5"
-                        htmlFor="option3-outlined"
-                    >
-                        50-100lbs
-                    </label>
-                    <input
-                        onChange={() => setWeight("100+ lbs")}
-                        type="radio"
-                        value="100+ lbs"
-                        className="btn-check"
-                        name="weight-options"
-                        id="option4-outlined"
-                        autoComplete="off"
-                    />
-                    <label
-                        className="btn btn-outline-warning mb-3 px-5"
-                        htmlFor="option4-outlined"
-                    >
-                        100lbs+
-                    </label>
                 </div>
-            </div>
-            <div className="col-md-6">
-                <div className="gender-buttons text-center mb-3">
-                    <p>Gender</p>
-                    <input
-                        onChange={() => setGender("Male")}
-                        type="radio"
-                        value="Male"
-                        className="btn-check"
-                        name="gender-options"
-                        id="male-outlined"
-                        autoComplete="off"
-                    />
-                    <label
-                        className="btn btn-outline-warning mb-3 me-5 px-5"
-                        htmlFor="male-outlined"
-                    >
-                        Male
+                <div className="col-md-12">
+                    <label htmlFor="birthday" className="form-label">
+                        Date of Birth
                     </label>
                     <input
-                        onChange={() => setGender("Female")}
-                        type="radio"
-                        value="Female"
-                        className="btn-check"
-                        name="gender-options"
-                        id="female-outlined"
-                        autoComplete="off"
+                        onChange={(e) => setBirth(e.target.value)}
+                        type="date"
+                        className="form-control"
+                        id="birthday"
                     />
-                    <label
-                        className="btn btn-outline-warning mb-3 px-5"
-                        htmlFor="female-outlined"
-                    >
-                        Female
-                    </label>
                 </div>
-            </div>
-            <div className="col-md-6">
-                <div className="spayed-neutered text-center mb-3">
-                    <p>Spayed / Neutered</p>
-                    <input
-                        onChange={() => setSpayedNeutered("Yes")}
-                        type="radio"
-                        value="Yes"
-                        className="btn-check"
-                        name="spayedNeutered-options"
-                        id="yes-outlined"
-                        autoComplete="off"
-                    />
-                    <label
-                        className="btn btn-outline-warning mb-3 me-5 px-5"
-                        htmlFor="yes-outlined"
-                    >
-                        Yes
-                    </label>
-                    <input
-                        onChange={() => setSpayedNeutered("No")}
-                        type="radio"
-                        value="No"
-                        className="btn-check"
-                        name="spayedNeutered-options"
-                        id="no-outlined"
-                        autoComplete="off"
-                    />
-                    <label
-                        className="btn btn-outline-warning mb-3 px-5"
-                        htmlFor="no-outlined"
-                    >
-                        No
-                    </label>
+                <div className="col-md-12">
+                    <div className="weight-buttons mb-3 text-center">
+                        <p className="buttonsTitle">Weight</p>
+                        <input
+                            onChange={() => setWeight("0-25 lbs")}
+                            type="radio"
+                            value="0-25 lbs"
+                            className="btn-check"
+                            name="weight-options"
+                            id="option1-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning me-3 mb-3 px-5"
+                            htmlFor="option1-outlined"
+                        >
+                            0-25lbs
+                        </label>
+                        <input
+                            onChange={() => setWeight("25-50 lbs")}
+                            type="radio"
+                            value="25-50 lbs"
+                            className="btn-check"
+                            name="weight-options"
+                            id="option2-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning me-3 mb-3 px-5"
+                            htmlFor="option2-outlined"
+                        >
+                            25-50lbs
+                        </label>
+                        <input
+                            onChange={() => setWeight("50-100 lbs")}
+                            type="radio"
+                            value="50-100 lbs"
+                            className="btn-check"
+                            name="weight-options"
+                            id="option3-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning me-3 mb-3 px-5"
+                            htmlFor="option3-outlined"
+                        >
+                            50-100lbs
+                        </label>
+                        <input
+                            onChange={() => setWeight("100+ lbs")}
+                            type="radio"
+                            value="100+ lbs"
+                            className="btn-check"
+                            name="weight-options"
+                            id="option4-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning mb-3 px-5"
+                            htmlFor="option4-outlined"
+                        >
+                            100lbs+
+                        </label>
+                    </div>
                 </div>
-            </div>
-            <div className="col-md-12 text-center">
-                <button
-                    onClick={(e) => handleSubmit(e)}
-                    type="submit"
-                    className="btn btn-primary btn-lg saveButton me-5"
-                >
-                    Save
-                </button>
-                <Link to={`/private`}>
-                    <button type="submit" className="btn btn-danger btn-lg cancelButton">
-                        Cancel
+                <div className="col-md-6">
+                    <div className="gender-buttons text-center mb-3">
+                        <p className="buttonsTitle">Gender</p>
+                        <input
+                            onChange={() => setGender("Male")}
+                            type="radio"
+                            value="Male"
+                            className="btn-check"
+                            name="gender-options"
+                            id="male-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning mb-3 me-5 px-5"
+                            htmlFor="male-outlined"
+                        >
+                            Male
+                        </label>
+                        <input
+                            onChange={() => setGender("Female")}
+                            type="radio"
+                            value="Female"
+                            className="btn-check"
+                            name="gender-options"
+                            id="female-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning mb-3 px-5"
+                            htmlFor="female-outlined"
+                        >
+                            Female
+                        </label>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="spayed-neutered text-center mb-3">
+                        <p className="buttonsTitle">Spayed / Neutered</p>
+                        <input
+                            onChange={() => setSpayedNeutered("Yes")}
+                            type="radio"
+                            value="Yes"
+                            className="btn-check"
+                            name="spayedNeutered-options"
+                            id="yes-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning mb-3 me-5 px-5"
+                            htmlFor="yes-outlined"
+                        >
+                            Yes
+                        </label>
+                        <input
+                            onChange={() => setSpayedNeutered("No")}
+                            type="radio"
+                            value="No"
+                            className="btn-check"
+                            name="spayedNeutered-options"
+                            id="no-outlined"
+                            autoComplete="off"
+                        />
+                        <label
+                            className="btn btn-outline-warning mb-3 px-5"
+                            htmlFor="no-outlined"
+                        >
+                            No
+                        </label>
+                    </div>
+                </div>
+                <div className="col-md-12 text-center">
+                    <button
+                        onClick={(e) => handleSubmit(e)}
+                        type="submit"
+                        className="btn btn-lg saveButton me-5"
+                    >
+                        Save
                     </button>
-                </Link>
-            </div>
-        </form>
+                    <Link to={`/private`}>
+                        <button type="submit" className="btn btn-danger btn-lg cancelButton">
+                            Cancel
+                        </button>
+                    </Link>
+                </div>
+            </form>
+        </div>
     );
 };
 
