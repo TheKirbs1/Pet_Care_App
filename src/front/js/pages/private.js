236163abeef923530fda11dd0fe3505ac166d37f
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-07-23 06:37:27 +0000

    changed gradient color on private page header and card, changed button colors

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -68,6 +68,7 @@ export const Private = () => {
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
                                                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
                                                 </svg>
+                                                <p>Edit profile</p>
                                             </button>
                                         </Link>
                                     </div>
@@ -86,7 +87,7 @@ export const Private = () => {
                                     <div className="col-md-4">
                                         <div className="right-buttons">
                                             <Link to="pet_registration">
-                                                <button className="btn mt-4 add-pet text-primary">
+                                                <button className="btn mt-4 add-pet">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                                                     </svg>
@@ -97,7 +98,7 @@ export const Private = () => {
                                                 if (selectedIndex === user.dogs.length - 1) {
                                                     setSelectedIndex(0)
                                                 } else { setSelectedIndex(selectedIndex + 1) }
-                                            }} className="btn text-primary switch-button">
+                                            }} className="btn switch-button mt-3">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-left-right" viewBox="0 0 16 16">
                                                     <path fillRule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 1 1 .708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5" />
                                                 </svg>
@@ -143,7 +144,7 @@ export const Private = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                                         </svg>
-                                        <span className="ml-2"> Add a Pet</span>
+                                        <span className="ml-2 ms-2"> Add a Pet</span>
                                     </button>
                                 </Link>
                             </div>
